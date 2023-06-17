38a03c1494d5e7b1dbe87b1b7e7fcd84edb31994
Torajonov <torajonov3112@gmail.com>
2023-06-17 14:51:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ def upload_file():
             return redirect(url_for('predict', filename = filename))
     return render_template('index.html') 
 
-
 @app.route('/predict/<filename>')
 def predict(filename):
     detection = Detection()
